3482a07eaeebe2ea2ec6a0965f15d35006ce8801
princess esor <princessntomo@gmail.com>
2022-11-03 09:54:21 +0200

    Add project files.

---
+++ 2LAB1/Class1.cs
@@ -0,0 +1,52 @@
+﻿//class.cs
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2LAB1
+{
+  abstract public class Class1
+    {
+        Dentist Den = new Dentist();
+        storyteller storyteller = new storyteller();
+
+        public class Student
+        {
+            public string line;
+            public string firstname = "Firstname: Princes";
+            public string lastname = "Lastname: Esor";
+            public int yearofstudy = 2;
+            public int id = 200568;
+            public string gender = "female";
+            public int avgscore = 75;
+
+           public void cook()
+            {
+                Console.WriteLine(firstname + " can cook");
+            }
+        }
+           public static void cook()
+            {
+                Console.WriteLine("i love cooking");
+            }
+       
+    }
+    public class Dentist : Class1
+    {
+        public string name = "Mr x";
+        int age = 45;
+        public string car = "mercedes";
+    }
+
+    public class storyteller : Class1
+    {
+        public string name = "Mrs x";
+        int age = 37;
+        public string car = "rolls royce";
+    }
+
+}
---
+++ 2LAB1/Program.cs
@@ -0,0 +1,47 @@
+﻿//program.cs
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader input_file = null;
+            input_file = new StreamReader("princessfile.txt");
+
+            StreamWriter output_file = new StreamWriter("princessfile.txt.out");
+
+            string line = "";
+
+            List<Class1.Student> elements = new List<Class1.Student>();
+
+            for (int line_no = 1; (line = input_file.ReadLine()) != null; ++line_no)
+            {
+                Class1.Student element = new Class1.Student
+                {
+                    line = line
+                };
+                elements.Add(element);
+            }
+
+            foreach (Class1.Student iter in elements)
+            {
+                Console.WriteLine(iter.line);
+                output_file.WriteLine(iter.line);
+            }
+            if (input_file != null)
+            {
+                input_file.Close();
+                output_file.Close();
+            }
+
+            Class1.cook();
+
+            Console.ReadLine();
+        }
+    } 
+}
